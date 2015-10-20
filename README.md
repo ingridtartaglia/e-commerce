ce6554c81bae8039af35da8b6606429c9970646d
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2015-10-19 22:53:01 -0200

    html and css

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 ---
 Technologies used:
 - HTML
-- CSS
+- CSS (SASS)
 - JavaScript (AngularJS)
 - Bootstrap
 - Bower
+
+Icons by http://www.freepik.com from http://www.flaticon.com. Licensed under Creative Commons by 3.0.
--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('commerce', function($scope){
-    
+    $scope.drinks = [
+        {
+            img: "imgs/lemon.png",
+            title: "Deli Lemonade",
+            description: "Lemon, ginger and mint",
+            price: "5"
+        },
+        {
+            img: "imgs/grape.png",
+            title: "Deli Grape",
+            description: "Grape, apple and basil",
+            price: "5"
+        },
+        {
+            img: "imgs/carrot.png",
+            title: "Deli Carrot",
+            description: "Carrot, orange and mint",
+            price: "5"
+        },
+        {
+            img: "imgs/strawberry.png",
+            title: "Deli Strawberry",
+            description: "Strawberry, pineapple and basil",
+            price: "6"
+        },
+        {
+            img: "imgs/watermelon.png",
+            title: "Deli Watermelon",
+            description: "Watermelon, pepper and cucumber",
+            price: "7"
+        },
+        {
+            img: "imgs/pineapple.png",
+            title: "Deli Pineapple",
+            description: "Pineapple, coconut and vanilla",
+            price: "7"
+        },
+    ];
+
 });
